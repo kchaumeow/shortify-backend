ad32bb6d5fe8d586a38008b8e4d9e05a6b28853f
kchaumeow <atrofimova516@gmail.com>
2023-08-17 10:42:58 +0300

    fix: rm cache

--- prisma/migrations/20230816122726_ini/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Url" (
-    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-    "link" TEXT NOT NULL,
-    "hashedLink" TEXT
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "Url_hashedLink_key" ON "Url"("hashedLink");
